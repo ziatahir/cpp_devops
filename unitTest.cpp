a919cf3b3be3bc564ff533943bc381e65a32eb3e
ziatahir <46965613+ziatahir@users.noreply.github.com>
2020-04-07 15:02:48 +0530

    Update and rename test.cpp to unitTest.cpp

--- unitTest.cpp
+++ unitTest.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE const_string test
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(dummy) {
-    BOOST_TEST(1 + 1 == 3);
+BOOST_AUTO_TEST_CASE(sample_test_case) {
+    BOOST_TEST(1 + 1 == 2);
 }
