46b210855e787df7486019c488694a79f062bf75
ziatahir <46965613+ziatahir@users.noreply.github.com>
2020-04-23 11:26:31 +0530

    Update unitTest.cpp

--- unitTest.cpp
+++ unitTest.cpp
@@ -1,11 +1,25 @@
-#define BOOST_TEST_MODULE const_string test
+#define BOOST_TEST_MODULE sampleUnitTest
 #include <boost/test/unit_test.hpp>
+#include <boost/algorithm/string.hpp>
 
 BOOST_AUTO_TEST_CASE(sample_test_case) {
     BOOST_TEST(1 + 1 == 2);
 }
 
-BOOST_AUTO_TEST_CASE(sample_test_case2) {
-    BOOST_TEST(1 + 1 == 3);
+//functions to check logic
+int addTwoNumbers(int i, int j ) {
+ return i+j;
 }
 
+int checksize(std::string str ) {
+ return str.size();
+}
+
+// test cases
+BOOST_AUTO_TEST_CASE(addition) {
+ BOOST_CHECK(addTwoNumbers(2, 3) == 5);
+}
+
+BOOST_AUTO_TEST_CASE(size) {
+ BOOST_CHECK(checksize("hello") == 5);
+}
